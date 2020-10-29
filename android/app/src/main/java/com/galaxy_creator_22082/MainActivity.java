4d930d2ed59e572641e75017c05d2338056c56e7
Crowdbotics <team@crowdbotics.com>
2020-10-29 11:41:53 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/galaxy_creator_22082/MainActivity.java
@@ -0,0 +1,15 @@
+package com.galaxy_creator_22082;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "galaxy_creator_22082";
+    }
+}
